5ff3fe7dae19817648b8372b7809caa65380b75e
Jan Schlacher <j.schlacher@trisoft.at>
2016-08-31 13:32:54 +0200

    changed schema from user to object in messages

--- test/model/find.js
+++ test/model/find.js
@@ -15,24 +15,33 @@ describe('find', function() {
     neo4js = serverHelper.getNeo4jsInstance();
     DbModel = neo4js.define('Find', schema);
     return neo4js.sync()
-      .then(() => DbModel.create({ text: 'asdf' }));
+      .then(() => DbModel.create({ text: 'asdf' }))
+      .then(() => DbModel.create({ text: 'foo' }))
+      .then(() => DbModel.create({ text: 'foo' }));
   });
 
   it('should return a promise', function() {
     expect(DbModel.find({ text: 'asdf' })).to.be.instanceof(Promise);
   });
 
-  it('should return an array of users', function() {
+  it('should return an array of objects', function() {
     return DbModel.find()
       .then(objects => {
         expect(objects).to.be.instanceof(Array);
       });
   });
 
-  it('should return exactly one user', function() {
+  it('should return all 3 created objects', function() {
     return DbModel.find()
       .then(objects => {
-        expect(objects.length).to.equal(1);
+        expect(objects.length).to.equal(3);
+      });
+  });
+
+  it('should return exactly one object with text = "foo"', function() {
+    return DbModel.find({ text: 'foo' })
+      .then(objects => {
+        expect(objects.length).to.equal(2);
       });
   });
 });
